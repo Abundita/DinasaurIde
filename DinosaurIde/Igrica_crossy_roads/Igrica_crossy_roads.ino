411b1f9df978beb57a9fad13e4e244113d81a91f
ficor07 <109144263+ficor07@users.noreply.github.com>
2022-07-12 12:59:31 +0200

    slo

--- DinosaurIde/Igrica_crossy_roads/Igrica_crossy_roads.ino
+++ DinosaurIde/Igrica_crossy_roads/Igrica_crossy_roads.ino
@@ -3,8 +3,7 @@
 #define TFT_CS  5
 #define TFT_DC 21
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// test
-// test 2
+
 // tipke
 int TipkaLR = 34;
 int TipkaUD = 35;
@@ -105,6 +104,42 @@ void loop() {
   Serial.println  (StanjeTipkalaUD);
   if (StanjeTipkala >= 1300 and StanjeTipkala <= 2100){
       X = X + 20;
+      Xglava =  Xglava + 20;
+      Yglava =  Yglava + 20;
+       Xoko = Xoko + 20;
+       Yoko = Yoko + 20;
+       Xusta1 = Xusta1 + 20;
+        Yusta1 + 20;
+        Xusta2 + 20;
+        Yusta2 + 20;
+        Xkapa1 + 20;
+        Ykapa1 + 20;
+        Xkapa2 + 20;
+        Ykapa2 + 20;
+        Xnoga1 + 20;
+        Ynoga1 + 20;
+        Xnoga2 + 20;
+        Ynoga2 + 20;
+        Xrep + 20;
+        Yrep + 20;
+        Xtrokut1.1 + 20;
+        Ytrokut1.1 + 20;
+        Xtrokut1.2 + 20;
+        Ytrokut1.2 + 20;
+        Xtrokut1.3 + 20;
+        Ytrokut1.3 + 20;
+        Xtrokut2.1 + 20;
+        Ytrokut2.1 + 20;
+        Xtrokut2.2 + 20;
+        Ytrokut2.2 + 20;
+        Xtrokut2.3 + 20;
+        Ytrokut2.3 + 20;
+        Xtrokut3.1 + 20;
+        Ytrokut3.1 + 20;
+        Xtrokut3.2 + 20;
+        Ytrokut3.2 + 20;
+        Xtrokut3.3 + 20;
+        Ytrokut3.3 
       delay(200);
       }
       StanjeTipkalaUD = analogRead(TipkaUD);
