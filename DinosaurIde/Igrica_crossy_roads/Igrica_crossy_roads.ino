cbf3e174643d078670d28cce33396b764a288fa4
ficor07 <109144263+ficor07@users.noreply.github.com>
2022-07-12 16:15:41 +0200

    n

--- DinosaurIde/Igrica_crossy_roads/Igrica_crossy_roads.ino
+++ DinosaurIde/Igrica_crossy_roads/Igrica_crossy_roads.ino
@@ -146,40 +146,43 @@ void setup() {
   Serial.begin (9600);
   tft.begin();
   tft.fillScreen(ILI9341_BLACK);
+  int dino(){
+    //character dinosaur
+    tft.setTextSize(5);
+    tft.setRotation(3);
+    
+    //tijelo i glava
+    tft.fillRect(X, Y, 20, 20, ILI9341_GREEN); 
+    tft.fillRect(Xglava, Yglava, 12, 12, ILI9341_GREEN);
+    
+    //oko i usta
+    tft.fillRect(Xoko, Yoko, 2, 2, ILI9341_BLACK);
+    tft.drawLine(Xusta1, Yusta1, 45, 111, ILI9341_BLACK);
+    tft.fillRect(Xusta2, Yusta2, 1.5, 1, ILI9341_BLACK);
+    
+    //kapa
+    tft.drawLine(Xkapa1, Ykapa1, 44, 101, ILI9341_RED);
+    tft.fillRect(Xkapa2, Ykapa2, 10, 7.5, ILI9341_RED);
   
-  //character dinosaur
-  tft.setTextSize(5);
-  tft.setRotation(3);
+    //noge
+    tft.fillRect(Xnoga1, Ynoga1, 5, 5, ILI9341_GREEN);
+    tft.fillRect(Xnoga2, Ynoga2, 5, 5, ILI9341_GREEN);
   
-  //tijelo i glava
-  tft.fillRect(X, Y, 20, 20, ILI9341_GREEN); 
-  tft.fillRect(Xglava, Yglava, 12, 12, ILI9341_GREEN);
-  
-  //oko i usta
-  tft.fillRect(Xoko, Yoko, 2, 2, ILI9341_BLACK);
-  tft.drawLine(Xusta1, Yusta1, 45, 111, ILI9341_BLACK);
-  tft.fillRect(Xusta2, Yusta2, 1.5, 1, ILI9341_BLACK);
-  
-  //kapa
-  tft.drawLine(Xkapa1, Ykapa1, 44, 101, ILI9341_RED);
-  tft.fillRect(Xkapa2, Ykapa2, 10, 7.5, ILI9341_RED);
- 
-  //noge
-  tft.fillRect(Xnoga1, Ynoga1, 5, 5, ILI9341_GREEN);
-  tft.fillRect(Xnoga2, Ynoga2, 5, 5, ILI9341_GREEN);
- 
-  //rep
-  tft.fillRect(Xrep, Yrep, 11, 4.5, ILI9341_GREEN);
-  
-  //bodlje na leđima
-  tft.drawTriangle(Xtrokut11, Ytrokut11, Xtrokut12, Ytrokut12, Xtrokut13, Ytrokut13, ILI9341_WHITE);
-  tft.drawTriangle(Xtrokut21, Ytrokut21, Xtrokut22, Ytrokut22, Xtrokut23, Ytrokut23, ILI9341_WHITE);
-  tft.drawTriangle(Xtrokut31, Ytrokut31, Xtrokut32, Ytrokut32, Xtrokut33, Ytrokut33, ILI9341_WHITE);
+    //rep
+    tft.fillRect(Xrep, Yrep, 11, 4.5, ILI9341_GREEN);
+    
+    //bodlje na leđima
+    tft.drawTriangle(Xtrokut11, Ytrokut11, Xtrokut12, Ytrokut12, Xtrokut13, Ytrokut13, ILI9341_WHITE);
+    tft.drawTriangle(Xtrokut21, Ytrokut21, Xtrokut22, Ytrokut22, Xtrokut23, Ytrokut23, ILI9341_WHITE);
+    tft.drawTriangle(Xtrokut31, Ytrokut31, Xtrokut32, Ytrokut32, Xtrokut33, Ytrokut33, ILI9341_WHITE);
+  } 
+  dino();
 }
 
 void loop() {
   // Kretanje
   tft.fillScreen(ILI9341_BLACK);
+  dino();
   StanjeTipkala = analogRead(TipkaLR);
   StanjeTipkalaUD = analogRead(TipkaUD);
   Serial.println (StanjeTipkala);
